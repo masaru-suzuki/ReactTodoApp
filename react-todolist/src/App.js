cea3d62d6d7e2aba97bedbf398d843a037d423d3
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2020-01-14 05:25:10 +0900

    sort

--- react-todolist/src/App.js
+++ react-todolist/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+//shortIdって何？
 import shortId from 'shortid'
 import './App.css'
 import Board from './Board.js'
@@ -7,25 +8,31 @@ import { getDefaultTodoItem } from './todo-list'
 
 class App extends React.Component {
   state = {
+    sortByDeadlineBtn: false,
+    sorByImportanceBtn: false,
     todoList: [],
     newTodo: getDefaultTodoItem(),
   }
 
   componentDidMount = () => this._initTodoList()
 
-  handleChange = event =>
+  handleChange = event => {
     this.setState({
       newTodo: {
+        //...ってどういう使い方なの？newTodoが複数あるときのため？
         ...this.state.newTodo,
+        //このまとめ方できるようになりたい
         [event.target.name]: event.target.value,
       },
     })
+  }
 
   addTodoList = event => {
     event.preventDefault()
     const newTodo = { ...this.state.newTodo, id: shortId() }
     this._updateTodoListAndLS([...this.state.todoList, newTodo])
     this._resetForm()
+    console.log(this.state.todoList)
   }
 
   checkAction = index => {
@@ -38,6 +45,26 @@ class App extends React.Component {
     }
     newTodoList[index] = updatedTodo
     this._updateTodoListAndLS(newTodoList)
+  }
+
+  sortByDeadline = () => {
+    const newTodoList = [...this.state.todoList];
+    const sortedTodoList = newTodoList.slice().sort(function(a, b) {
+      if(a.deadline < b.deadline){
+        return -1;
+    }else if(a.deadline > b.deadline){
+        return 1;
+    }
+    return 0;
+    })
+    if(this.state.sortByDeadlineBtn){
+      this.setState({todoList: sortedTodoList,sortByDeadlineBtn: false})
+    }else {
+      this.setState({todoList: newTodoList,sortByDeadlineBtn: true})
+    }
+    console.log(sortedTodoList);
+    console.log(this.state.todoList);
+    console.log(this.state.sortByDeadlineBtn)
   }
 
   clearCompletedTask = () => {
@@ -62,17 +89,30 @@ class App extends React.Component {
           deleteRow={this.deleteRow}
           checkAction={this.checkAction}
           clearCompletedTask={this.clearCompletedTask}
+          sortByDeadline = {this.sortByDeadline}
+          sorByImportance = {this.sorByImportance}
         />
       </div>
     )
   }
-
+//_をつけているのはなんで？=>privateの意味
   _initTodoList = () => {
     this.setState({ todoList: JSON.parse(localStorage.getItem('todoList')) || [] })
   }
+  _compareFunc = (a, b) => {
+    if(a.deadline < b.deadline){
+      return -1;
+  }else if(a.deadline > b.deadline){
+      return 1;
+  }
+  return 0;
+  }
 
   _updateTodoListAndLS = todoList => {
+    //処理を,を使って並列に並べることもできる
+    //なんでsetStateのなかでthis._syncLSをしたのか？
     this.setState({ todoList }, this._syncLS)
+    console.log(this.state.todoList)
   }
 
   _syncLS = () => {
