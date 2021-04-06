c076eb6f9c5424778f777f6c92faa072d2d22481
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2021-04-06 13:17:56 +0900

    [chore] yarn up grade

--- react-todolist/src/Row.js
+++ react-todolist/src/Row.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React from 'react'
 
 function Row(props) {
-  return(
+  return (
     <tr className={props.todo.isDone ? 'checked' : ''}>
-      <td><input type="checkbox" className="checkbox" checked={props.todo.isDone} onChange={()=>{{props.checkAction(props.index)}}}/></td>
+      <td>
+        <input
+          type="checkbox"
+          className="checkbox"
+          checked={props.todo.isDone}
+          onChange={() => {
+            {
+              props.checkAction(props.index)
+            }
+          }}
+        />
+      </td>
       <td>{props.todo.title}</td>
       <td>{props.todo.deadline}</td>
       <td>{props.todo.importance}</td>
       <td>{props.todo.completedAt}</td>
-      <td><a href="#" className="btn btn-secondary btn-sm delete" onClick={() => props.deleteRow(props.index)}>X</a></td>
-      </tr>
+      <td>
+        <a
+          href="#"
+          className="btn btn-secondary btn-sm delete"
+          onClick={() => props.deleteRow(props.index)}
+        >
+          X
+        </a>
+      </td>
+    </tr>
   )
 }
 
